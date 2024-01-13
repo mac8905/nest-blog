4b86a9014bcba78b706ee13f4a097dbdeafc51df
Rubiela Murcia <rubymurcia88@gmail.com>
2024-01-12 22:16:58 -0500

    Creating users and posts modules with zero functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:root-e3f9dedc-30d9-4447-8f31-efbe525a61fd@localhost/nest-blog',
+    ),
+    UsersModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
